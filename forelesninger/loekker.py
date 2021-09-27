03506e694fe5221993251b0c8150996c46403b42
Petter Åslie <paaslie@hotmail.com>
2021-09-27 22:50:55 +0200

    oving 5

--- forelesninger/loekker.py
+++ forelesninger/loekker.py
@@ -1,9 +1,8 @@
 import random
-loekker.py
-tall = random, randint(1, 100)
+tall = randint(1, 100)
 
-while gjett != tall
-gjett = int(input('Gjett et tall mellom 1 og 100 :'))
+while gjett != tall:
+    gjett = int(input('Gjett et tall mellom 1 og 100 :'))
 if gjett == tall:
     print('Riktig!')
 elif gjett > tall:
