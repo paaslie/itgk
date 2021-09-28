2aee8b7c62e25df7a97bbb09b7e9608d478557b1
Petter Åslie <paaslie@hotmail.com>
2021-09-29 00:36:47 +0200

    itgk

--- forelesninger/loekker.py
+++ forelesninger/loekker.py
@@ -1,9 +1,12 @@
 import random
-tall = randint(1, 100)
+tall = random.randint(1, 100)
+gjett = int(input('Gjett et tall mellom 1 og 100 :'))
 
 while gjett != tall:
     gjett = int(input('Gjett et tall mellom 1 og 100 :'))
-if gjett == tall:
-    print('Riktig!')
-elif gjett > tall:
-    print('Det er feil.')
+    if gjett == tall:
+        print('Riktig!')
+    elif gjett > tall:
+        print('Det er feil.')
+print()
+
---
+++ teori/binaer.py
@@ -0,0 +1,5 @@
+a= float(0.1) 
+b= float(0.2)
+
+p = (a+b)
+print(p)
