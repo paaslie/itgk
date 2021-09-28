256dab5f9cbd5c989aca58eabefba1e2c5495b1b
Petter Åslie <paaslie@hotmail.com>
2021-09-29 00:41:32 +0200

    itgk

--- forelesninger/lokker/lokker.py
+++
@@ -1,9 +0,0 @@
-import random
-tall = random, randint(1, 100)
-
-while gjett != tall
-gjett = int(input('Gjett et tall mellom 1 og 100 :'))
-if gjett == tall:
-    print('Riktig!')
-elif gjett > tall:
-    print('Det er feil.')
