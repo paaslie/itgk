007c886526cacd428391533dddb699f6e3ba03a3
Petter Åslie <paaslie@hotmail.comm>
2025-01-26 08:35:47 +0100

    oppgave 1

---
+++ itgkeksamen/1.py
@@ -0,0 +1,11 @@
+#1a - komprimering (5%)
+#Denne oppgaven har to deler:
+# 1.Forklar forskjellen på Lossy og Lossless komprimering.
+# 2.Gi et eksempel på hvordan "run-length encoding" kan brukes til å komprimere noe.
+
+# Forskjellen på lossy komprimering og lossless komprimering er at lossy fjerner unødvendiginformasjon som er vanskelig for blåtte øye å oppdage 
+# mens lossless gjenskaper fullverdigdata som komprimeres.
+# En lossy komprimering vil komprimere tilsvarende lik informasjon,feks like pixler i et bilde eller tar de samme nyansene i en video.
+# 
+# Run-length encoding kan brukes ved at man komprimerer flere like data samlet. Feks et bildemed størst andel svart skjerm og et gult smilefjes i midten 
+# gjøres om til korte koder somtilsvarer rekker med kortere verdier som tilsvarer det fulle bildet. Dette gjør det mulig å oppnålossless komprimering.
